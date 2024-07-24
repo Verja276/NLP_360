0bdac149300f492ea3b2976f12a379b369ade5a0
minooshayan97 <minooshayan97@gmail.com>
2024-07-24 11:46:37 -0700

    initial and introductory msg

--- TestWithUI.py
+++ TestWithUI.py
@@ -9,39 +9,43 @@ from set_key import set_api_key_from_file
 
 
 def HCS(message, history):
-    initial_msg = "You are talking to SFU counselling information Chatbot. If you wish to exit the conversation, please type in the word: exit"
-    if len(history) == 0:
-        print("Bot>> " + initial_msg, file=f)
-        print(file=f)
-        return initial_msg
-    else:
-        augmented_prompt = manage_query(message)
-        ai_message = bot.handle_input(HumanMessage(content=augmented_prompt))
 
-        print("User>>" + message, file=f)
-        print(file=f)
-        print("Bot>> " + ai_message.content, file=f)
-        print(file=f)
+    augmented_prompt = manage_query(message)
+    ai_message = bot.handle_input(HumanMessage(content=augmented_prompt))
+     
+    f = open(f"UIchatlog/chat1.txt", "a")
+    print("User>>" + message, file=f)
+    print(file=f)
+    print("Bot>> " + ai_message.content, file=f)
+    print(file=f)
+    f.close()
 
-        return ai_message.content
+    return ai_message.content
 
 
-
-f = open(f"UIchatlog/chat1.txt", "a")
 set_api_key_from_file()
 
 # Initiate chatbot
 bot_sysMessage = "You are an information chatbot to answer students' questions based on content that is given to you from SFU counselling website."
 bot = Bot(bot_sysMessage)
 
+description = """I'm here to help you find information about the counseling services available at SFU. Whether you have questions about how to book an appointment, what types of services we offer, or where to find self-help resources, I'm here to guide you to the right webpages.
+
+Please note that I am not a counselor and cannot provide counseling services or handle emergencies. If you're experiencing a crisis, please contact SFU's crisis services directly or dial emergency services.
+
+Your privacy is important to us. All conversation histories will be deleted once our chat ends, ensuring that no one can access them.
+
+Feel free to ask me anything about SFU's Health & Counseling Services, and I'll do my best to assist you!"""
+
+initial_msg = "Ask me a question about SFU Health & Counseling Service."
+
 gr.ChatInterface(
     HCS,
     chatbot=gr.Chatbot(height=500),
-    textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7),
-    title="title",
-    description="description",
+    textbox=gr.Textbox(placeholder=initial_msg, container=False, scale=7),
+    title="Welcome to the SFU Health & Counseling Services Chatbot!",
+    description=description,
     theme="soft",
-    #examples=["Hello", "Am I cool?", "Are tomatoes vegetables?"],
     cache_examples=False,
     retry_btn=None,
     undo_btn=None,
